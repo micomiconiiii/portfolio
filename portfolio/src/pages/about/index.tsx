1765409b451559a997986aa861cfaa379ee22a46
micomiconiiii <jhondelmico.abas@tup.edu.ph>
2025-11-17 12:58:31 +0800

    Fixed Mico Asset Image

--- portfolio/src/pages/about/index.tsx
+++ portfolio/src/pages/about/index.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import MicoProfileImage from '../../assets/mico-pxm.jpg'; // <-- 1. Import the image
+import MicoProfileImage from '../../assets/mico-pxm.png';
 
 
 const DownloadIcon = () => (
